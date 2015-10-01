f9c740cfc672334c4ef579a2b8e7c31fe9760a21
David Mercier <david.mercier@cdlinc.ca>
2015-09-30 21:31:54 -0400

    Click fonctionne

--- tasks.js
+++ tasks.js
@@ -4,21 +4,47 @@
 
 $(document).ready(function() {
 
-    $("#getTasks").click(function() {
+    $("#add").click(function () {
 
         var request = $.ajax({
-            url : 'http://127.0.0.1:5000/tasks',
-            type : 'GET'
+            url: 'http://127.0.0.1:5000/tasks',
+            type: 'GET'
         });
-        request.done(function(data) {
 
-            //result.d[0].EmployeeTitle
+        request.done(function (data) {
+            $('#noteContainerContent').empty();
 
+            data.tasks.forEach(function(task) {
+                var html = generateNote(task.id, task.task);
+                $("#noteContainerContent").append(html);
+            });
+
+            bindClickForNotes();
         });
-        request.fail(function(jqXHR, textStatus) {
+
+        request.fail(function (jqXHR, textStatus) {
             console.log(textStatus);
             alert("fail");
         });
     });
 
+    $("#modify").click(function() {
+
+    });
+
+    $("#delete").click(function() {
+
+    });
 });
+
+function generateNote(id, task) {
+    var html = "<div id='" + id + "' class='noteButton'>" + task + "</div>";
+    return html;
+}
+
+function bindClickForNotes() {
+    $(document).on("click", ".noteButton", function(){
+        alert("click on " + this.id);
+
+    });
+}
